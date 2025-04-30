d96d0c3c2e4eb181c2940a71924a330e49cb0aff
randy-bickham <163199482+randy-bickham@users.noreply.github.com>
2025-04-30 16:10:52 -0500

    Fixed missing modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <div className="App">
@@ -15,7 +12,7 @@ function App() {
       <h1>Hello from Amplify</h1>
     </header>
   </div>
-
+     
     </>
   )
 }
