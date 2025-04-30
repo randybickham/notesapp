35d5843c97dabb718fb5fd3629e2830e05224899
randy-bickham <163199482+randy-bickham@users.noreply.github.com>
2025-04-30 16:21:08 -0500

    copy paste

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
   return (
-    <>
     <div className="App">
-    <header className="App-header">
-      <img src={reactLogo} className="logo react" alt="React logo" />
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
 
-      <h1>Hello from Amplify</h1>
-    </header>
-  </div>
-     
-    </>
-  )
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
